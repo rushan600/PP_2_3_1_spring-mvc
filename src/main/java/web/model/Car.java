63bc5f37d7f9b9794ec812b350a98486520bb63a
rusha <rushan600@gmail.com>
2025-01-31 13:20:51 +0500

    Initial commit

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -6,6 +6,7 @@ public class Car {
     private int year;
 
     public Car(String model, String color, int year) {
+
         this.model = model;
         this.color = color;
         this.year = year;
