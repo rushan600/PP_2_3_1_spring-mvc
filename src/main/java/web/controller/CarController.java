c8ff4279efef267a98a44088194782f7ef53bce9
rusha <rushan600@gmail.com>
2025-01-31 13:20:51 +0500

    Initial commit

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,18 +3,24 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import web.model.Car;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    @GetMapping(value = "/car")
-    public String printWelcome(ModelMap model) {
-        List<String> messages = new ArrayList<>();
-        messages.add("Hello!");
-        model.addAttribute("messages", messages);
-        return "index";
+    @GetMapping(value = "/cars")
+    public String getCars(ModelMap model) {
+        List<Car> cars = Arrays.asList(
+                new Car("car1", "black", 2000),
+                new Car("car1", "red", 2001),
+                new Car("car2", "green", 2002),
+                new Car("car3", "blue", 2003),
+                new Car("car4", "white", 2004)
+        );
+        model.addAttribute("cars", cars);
+        return "cars";
     }
 }
